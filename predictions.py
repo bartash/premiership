40b38ad07524e660d408c1de7f5a0432df5741be
Andrew Sherman <andrew@ohair-sherman.com>
2026-05-11 14:39:51 -0700

    verify 2024/2025 data

--- predictions.py
+++ predictions.py
@@ -175,6 +175,7 @@ def print_sorted_scores(scores):
 def main():
     # Load predictions, final results, and expected scores
     verify(predictions__csv='predictions1.csv', results='final1.csv', expected='expected1.csv')
+    verify(predictions__csv='predictions_2024_2025.csv', results='final_2024_2025.csv', expected='expected_2024_2025.csv')
 
     print(f"\nLoad 2024/2025")
 
